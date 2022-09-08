aa0821fec8643e1e16ee0dc8f53879c39a23c7c1
Shahzeb Khan <60987842+cybercur4@users.noreply.github.com>
2022-09-08 15:43:44 +0530

    Update Helloworld.java

--- Porgress/Week 1/DAY3/src/main/Helloworld.java
+++ Porgress/Week 1/DAY3/src/main/Helloworld.java
@@ -1,7 +1,49 @@
 package main;
 
 public class Helloworld {
-	public static void main(String args[]) {
-		System.out.println("HELLOWORLD");
+	public int max(int a, int b) {
+		if(a>b)
+			return a;
+		else
+			return b;
 	}
-}
+	public String switchh(int n) {
+		switch(n) {
+		case 1:
+			return "one";
+		
+		case 2:
+			return "two";
+		default:
+			return "Wrong";
+		}
+		}
+	public int sum(int n) {
+		int sume=0;
+		while(n!=0) {
+			sume+=n;
+			n--;
+		}
+		return sume;
+	}
+	public int forl(int n) {
+		int sume=0;
+		for(int i=0;i<n;i++) {
+		if(i==1)
+			continue;
+		else if(i==n-2)
+			break;
+		else
+			sume+=i;
+		}
+		return sume;
+	}
+	
+	public static void main(String args[]) {
+	Helloworld hw=new Helloworld();
+	System.out.println(hw.max(2,3));
+	System.out.println(hw.switchh(1));
+	System.out.println(hw.sum(5));
+	System.out.println(hw.forl(5));
+	}
+	}
