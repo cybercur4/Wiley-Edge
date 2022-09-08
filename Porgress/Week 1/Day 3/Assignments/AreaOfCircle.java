b8e2c726db37db566e30fcaf212ac7a27d1c0c80
Shahzeb Khan <60987842+cybercur4@users.noreply.github.com>
2022-09-08 20:36:07 +0530

    Create AreaOfCircle.java

---
+++ Porgress/Week 1/Day 3/Assignments/AreaOfCircle.java
@@ -0,0 +1,34 @@
+/*Create aclass circle with following details:
+Attributes:radius,area.
+Method: calculateArea.
+Create object of circle with given radius and calculate the area.
+(Use the concept of parameterized constructor, code recommendation and 'this' keyword)*/
+
+
+class Circle{
+	double radius;
+	Circle(double radius){
+		this.radius=radius;
+	}
+	double calculateArea() {
+		return 3.14*radius*radius;
+	}
+}
+
+public class Question1 {
+	public static void main(String args[]) {
+		double area;
+		double radius=10.5;
+		Circle circle1=new Circle(radius);
+		area=circle1.calculateArea();
+		System.out.println("Area of circle with radius "+radius+" is " +area);
+		radius=5.6;
+		Circle circle2=new Circle(radius);
+		area=circle2.calculateArea();
+		System.out.println("Area of circle with radius "+radius+" is " +area);
+		radius=8.23;
+		Circle circle3=new Circle(radius);
+		area=circle3.calculateArea();
+		System.out.println("Area of circle with radius "+radius+" is " +area);
+	}
+}
